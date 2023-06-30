d9d6dfb1239b7ea7253996da4da526e5be7c1d48
Iara Silva Maciel Camargos <iara.camargos@linx.com.br>
2023-06-30 17:08:12 -0300

    API Minimal

--- PizzaStore/Program.cs
+++ PizzaStore/Program.cs
@@ -1,5 +1,19 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Description = "Keep track of your tasks", Version = "v1" });
+});
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+  c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
+});
 
 app.MapGet("/", () => "Hello World!");
 
