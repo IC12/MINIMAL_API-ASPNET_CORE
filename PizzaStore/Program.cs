7bb45f9e6bd459f0522d7c061e9c8ff60e544268
Iara Silva Maciel Camargos <iara.camargos@linx.com.br>
2023-07-03 11:36:54 -0300

    APIMinimal

--- PizzaStore/Program.cs
+++ PizzaStore/Program.cs
@@ -1,20 +1,27 @@
 using Microsoft.OpenApi.Models;
+using PizzaStore.DB;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Description = "Keep track of your tasks", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API", Description = "Making the Pizzas you love", Version = "v1" });
 });
+
+var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
-  c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
+  c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
 });
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
+app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
+app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
+app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
+app.MapDelete("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
 
 app.Run();
--- README.md
+++ README.md
@@ -3,5 +3,10 @@
 .NET 6 precisa estar instalado na sua máquina
 
 - Para criar o projeto mínimo de API digite 'dotnet new web -o PizzaStore -f net6.0' no terminal da IDE;
-- Para usar o Swagger e a interface do usuário na API, instale o pacote Swashbuckle
+- Para usar o Swagger e a interface do usuário na API, instale o pacote Swashbuckle;
     (Digite 'dotnet add package Swashbuckle.AspNetCore --version 6.1.4')
+- No terminal digite 'dotnet run' para executar o aplicativo;
+- No navegador: vá até o ponto de extremidade swagger 'http://localhost:{PORT}/swagger'
+    (A PORT vc encontra no arquivo launchSettings.json)
+- Esse é o suporte para OpenAPI para a API mínima usando Swagger;
+- No terminal, tecle ctrl+C para encerrar o porgrama.
